8479cb71c7320167934c2d92dd76fc13d208449c
avkozyrev <avkozyrev@domclick.ru>
2026-01-08 22:32:41 +0300

    ai logic

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
+	"math/rand"
 	"strconv"
 )
 
@@ -65,6 +66,103 @@ func (p *PlayField) CheckIfWin() bool {
 	return false
 }
 
+func (p *PlayField) AiMakeMove(aiSymbol int) {
+	// always try to occupy center of the field
+	if p.fields[1][1] == 0 {
+		err := p.MakeStep(aiSymbol, 1, 1)
+		if err == nil {
+			return
+		}
+	}
+
+	// check if any of rows or cols could give victory to any of the sides
+	for rowIndex := 0; rowIndex < 3; rowIndex++ {
+		enemySymbolsInRow := 0
+		aiSymbolsInRow := 0
+		freeColIndex := -1
+
+		enemySymbolsInCol := 0
+		aiSymbolsInCol := 0
+		freeRowIndex := -1
+
+		for colIndex := 0; colIndex < 3; colIndex++ {
+			if p.fields[rowIndex][colIndex] == aiSymbol {
+				aiSymbolsInRow++
+			} else if p.fields[rowIndex][colIndex] == 0 {
+				freeColIndex = colIndex
+			} else {
+				enemySymbolsInRow++
+			}
+
+			localRowIndex := colIndex
+			localColIndex := rowIndex
+			if p.fields[localRowIndex][localColIndex] == aiSymbol {
+				aiSymbolsInCol++
+			} else if p.fields[localRowIndex][localColIndex] == 0 {
+				freeRowIndex = localRowIndex
+			} else {
+				enemySymbolsInCol++
+			}
+		}
+
+		if aiSymbolsInRow == 2 && freeColIndex != -1 {
+			err := p.MakeStep(aiSymbol, rowIndex, freeColIndex)
+			if err == nil {
+				return
+			}
+		}
+
+		if aiSymbolsInCol == 2 && freeRowIndex != -1 {
+			err := p.MakeStep(aiSymbol, freeRowIndex, rowIndex)
+			if err == nil {
+				return
+			}
+		}
+
+		if enemySymbolsInRow == 2 && freeColIndex != -1 {
+			err := p.MakeStep(aiSymbol, rowIndex, freeColIndex)
+			if err == nil {
+				return
+			}
+		}
+
+		if enemySymbolsInCol == 2 && freeRowIndex != -1 {
+			err := p.MakeStep(aiSymbol, freeRowIndex, rowIndex)
+			if err == nil {
+				return
+			}
+		}
+	}
+
+	// try to occupy corners of the field if they are free
+	corners := [][2]int{
+		{0, 0}, {0, 2}, {2, 0}, {2, 2},
+	}
+
+	for i := range corners {
+		newIndex := rand.Intn(len(corners))
+		corners[i], corners[newIndex] = corners[newIndex], corners[i]
+	}
+
+	for i := range corners {
+		err := p.MakeStep(aiSymbol, corners[i][0], corners[i][1])
+		if err == nil {
+			return
+		}
+	}
+
+	// just fill all left spots of the field
+	for {
+		row := rand.Intn(3)
+		col := rand.Intn(3)
+		err := p.MakeStep(aiSymbol, row, col)
+		if err == nil {
+			return
+		}
+	}
+
+}
+
 func (p *PlayField) Print() {
 	fmt.Println("  0   1   2")
 	for i := range p.fields {
@@ -119,15 +217,22 @@ game:
 		for _, player := range players {
 			for {
 				fmt.Println("Player " + PlayFieldSymbols[player] + " go!")
-				row := getUserInputCircle("Row")
-				col := getUserInputCircle("Column")
 
-				err := p.MakeStep(player, row, col)
-				if err != nil {
-					fmt.Println(err)
-				} else {
+				if PlayFieldSymbols[player] == "O" {
+					p.AiMakeMove(player)
 					p.Print()
 					break
+				} else {
+					row := getUserInputCircle("Row")
+					col := getUserInputCircle("Column")
+
+					err := p.MakeStep(player, row, col)
+					if err != nil {
+						fmt.Println(err)
+					} else {
+						p.Print()
+						break
+					}
 				}
 			}
 			if p.CheckIfWin() {
